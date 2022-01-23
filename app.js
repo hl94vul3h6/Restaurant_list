daa2d2357098bfc96f498d43bbf3272801ec4b83
hl94vul3h6 <gary6780gary6780@gmail.com>
2022-01-22 22:58:09 -0800

    feat: add create function

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true,
 
 const db = mongoose.connection
 const port = 3000
+const bodyParser = require('body-parser')
 
 const exphbs = require('express-handlebars')
 // const restaurantList = require('./restaurant.json')
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 db.on('error', () => {
@@ -29,6 +30,16 @@ app.get('/', (req, res) => {
     .lean()
     .then(restaurantinfos => res.render('index', { restaurantinfos }))
     .catch(error => console.log(error))
+})
+
+app.get("/restaurants/new", (req, res) => {
+  res.render("new")
+})
+
+app.post("/restaurants", (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
 })
 
 app.get('/restaurants/:id', (req, res) => {
